48ca065c1ec50e24f3f6865ef35800ac650e5417
Eric Windmill <ewindmill@google.com>
2022-04-07 10:49:13 -0400

    adds flutter project shells

---
+++ analytics/android/app/src/main/kotlin/com/example/analytics/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.analytics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ functions/android/app/src/main/kotlin/com/example/functions/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.functions
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
