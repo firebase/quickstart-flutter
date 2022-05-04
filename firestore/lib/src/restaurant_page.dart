9fbb8f10453850102bfbd4842bf7b7455eb84438
Eric Windmill <ewindmill@google.com>
2022-05-04 10:08:13 -0700

    adds firestore app

---
+++ firestore/lib/src/restaurant_page.dart
@@ -0,0 +1,181 @@
+/*
+ *  Copyright 2022 Google LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import 'dart:math';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'data/restaurant_provider.dart';
+import 'model/restaurant.dart';
+import 'model/review.dart';
+import 'widgets/dialogs/review_create.dart';
+import 'widgets/restaurant_page_details.dart';
+import 'widgets/review_list_tile.dart';
+
+class RestaurantPage extends StatefulWidget {
+  static const route = '/restaurant';
+
+  final String restaurantId;
+
+  RestaurantPage({Key? key, required String restaurantId})
+      : restaurantId = restaurantId,
+        super(key: key);
+
+  @override
+  _RestaurantPageState createState() => _RestaurantPageState(restaurantId);
+}
+
+class _RestaurantPageState extends State<RestaurantPage> {
+  _RestaurantPageState(String restaurantId) {
+    _initAsyncData(restaurantId);
+  }
+
+  void _initAsyncData(String restaurantId) async {
+    final credential = await FirebaseAuth.instance.signInAnonymously();
+
+    final restaurant =
+        await _firestoreRestaurantProvider.getRestaurantById(restaurantId);
+    final reviews = await _firestoreRestaurantProvider
+        .getReviewsForRestaurant(restaurantId);
+
+    setState(() {
+      _user = credential.user!;
+      _restaurant = restaurant;
+      _reviews = reviews;
+      _isLoading = false;
+    });
+  }
+
+  late final Restaurant _restaurant;
+  late final User _user;
+  late List<Review> _reviews;
+  bool _isLoading = true;
+  final FirestoreRestaurantProvider _firestoreRestaurantProvider =
+      FirestoreRestaurantProvider();
+
+  void _onCreateReviewPressed(BuildContext context) async {
+    final newReview = await showDialog<Review>(
+      context: context,
+      builder: (BuildContext context) => ReviewCreateDialog(
+        userId: _user.uid,
+        userName: _user.displayName ?? 'Anonymous User',
+      ),
+    );
+    if (newReview != null) {
+      // Save the review
+      await _firestoreRestaurantProvider.addReview(
+        restaurantId: _restaurant.id!,
+        review: newReview,
+      );
+    }
+
+    _updateReviews();
+  }
+
+  void _onAddRandomReviewsPressed() async {
+    // Await adding a random number of random reviews
+    final numReviews = Random().nextInt(5) + 5;
+    for (var i = 0; i < numReviews; i++) {
+      await _firestoreRestaurantProvider.addReview(
+        restaurantId: _restaurant.id!,
+        review: Review.random(
+          userId: _user.uid,
+          userName: _user.displayName ?? 'Anonymous User',
+        ),
+      );
+    }
+
+    _updateReviews();
+  }
+
+  void _updateReviews() async {
+    final reviews = await _firestoreRestaurantProvider
+        .getReviewsForRestaurant(_restaurant.id!);
+    setState(() {
+      _reviews = reviews;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      floatingActionButton: _isLoading
+          ? null
+          : FloatingActionButton(
+              child: Icon(Icons.add),
+              tooltip: 'Tap to add a review',
+              onPressed: () => _onCreateReviewPressed(context),
+            ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Builder(
+              builder: (BuildContext context) {
+                return Center(
+                  child: Container(
+                    color: Colors.white,
+                    constraints: BoxConstraints(maxWidth: 900),
+                    alignment: Alignment.center,
+                    child: Column(
+                      children: [
+                        RestaurantDetails(restaurant: _restaurant),
+                        Divider(thickness: 1),
+                        _reviews.isNotEmpty
+                            ? Flexible(
+                                child: ListView.separated(
+                                  shrinkWrap: true,
+                                  itemCount: _reviews.length,
+                                  separatorBuilder: (context, index) =>
+                                      Divider(thickness: 1),
+                                  itemBuilder: (context, index) {
+                                    return ReviewListTile(
+                                      review: _reviews[index],
+                                    );
+                                  },
+                                ),
+                              )
+                            : Padding(
+                                padding: const EdgeInsets.only(top: 24.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Text(
+                                      'There are no reviews yet! Generate random reviews with button.',
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: _onAddRandomReviewsPressed,
+                                      child: Text('Add Random Reviews'),
+                                    )
+                                  ],
+                                ),
+                              ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
+
+class RestaurantPageArguments {
+  final String restaurantId;
+
+  RestaurantPageArguments(this.restaurantId);
+}
---
+++ firestore/lib/src/widgets/filter_bar.dart
@@ -0,0 +1,113 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+
+import '../model/filter.dart';
+
+const _boldStyle = TextStyle(fontWeight: FontWeight.bold);
+
+class FilterBar extends StatelessWidget {
+  FilterBar({
+    required VoidCallback onPressed,
+    required Filter filter,
+  })  : _onPressed = onPressed,
+        _filter = filter;
+
+  final VoidCallback _onPressed;
+  final Filter _filter;
+
+  List<InlineSpan> _buildCategorySpans(Filter filter) {
+    final noneSelected = filter.isDefault || filter.category == null;
+    return [
+      if (noneSelected) TextSpan(text: 'All Restaurants', style: _boldStyle),
+      if (!noneSelected) ...[
+        TextSpan(text: '${filter.category}', style: _boldStyle),
+        TextSpan(text: ' places'),
+      ],
+    ];
+  }
+
+  List<InlineSpan> _buildPriceSpans(Filter filter) {
+    return [
+      if (filter.price != null) ...[
+        TextSpan(text: ' of '),
+        TextSpan(text: '\$' * filter.price!, style: _boldStyle),
+      ],
+    ];
+  }
+
+  List<InlineSpan> _buildTitleSpans(Filter filter) {
+    return [
+      ..._buildCategorySpans(filter),
+      if (!filter.isDefault) ..._buildPriceSpans(filter),
+    ];
+  }
+
+  List<InlineSpan> _buildCitySpans(Filter filter) {
+    return [
+      if (filter.city != null) ...[
+        TextSpan(text: 'in '),
+        TextSpan(text: '${filter.city} ', style: _boldStyle),
+      ],
+    ];
+  }
+
+  List<InlineSpan> _buildSubtitleSpans(Filter filter) {
+    final orderedByRating = filter.sort == null || filter.sort == 'avgRating';
+    return [
+      ..._buildCitySpans(filter),
+      if (orderedByRating) TextSpan(text: 'by rating'),
+      if (!orderedByRating) TextSpan(text: 'by # reviews'),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: Colors.white,
+      padding: EdgeInsets.all(6),
+      onPressed: _onPressed,
+      child: Row(
+        children: [
+          Icon(Icons.filter_list),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(6, 0, 6, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    overflow: TextOverflow.ellipsis,
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.bodyText2,
+                      children: _buildTitleSpans(_filter),
+                    ),
+                  ),
+                  RichText(
+                    overflow: TextOverflow.ellipsis,
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.caption,
+                      children: _buildSubtitleSpans(_filter),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
