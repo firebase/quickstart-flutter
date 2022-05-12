b7ef80805156b5d59d5cce14a6614bec33e1bc8b
Eric Windmill <ewindmill@google.com>
2022-05-12 13:06:23 -0700

    removes empty projects

--- analytics/android/app/src/main/kotlin/com/example/analytics/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.analytics
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- functions/android/app/src/main/kotlin/com/example/functions/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.functions
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
