344f44d1088339c4078ba1a9a9b08b5ce2c172b6
Alexander Nohe <nohe@google.com>
2022-10-06 16:29:26 -0700

    Quickstart Finished

--- cloud_functions/lib/main.dart
+++ cloud_functions/lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseFunctions.instance.useFunctionsEmulator("localhost", 5001);
+
   runApp(const MyApp());
 }
 
@@ -65,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 )
                     .call(
                   {
-                    "word": _controller.value,
+                    "word": _controller.value.text,
                   },
                 ).then((result) {
                   setState(() {
@@ -88,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
