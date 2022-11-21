ac71a6cba8649d3f5526cd1566955da81040fc69
Alexander Nohe <nohe@google.com>
2022-11-21 09:55:54 -0800

    Add code license

--- inappmessaging/macos/Flutter/GeneratedPluginRegistrant.swift
+++ inappmessaging/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
+import firebase_app_installations
+import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FirebaseInstallationsPlugin.register(with: registry.registrar(forPlugin: "FirebaseInstallationsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
