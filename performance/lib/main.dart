0318fe91228335ad18b6e9d8312d0c8abb3fa959
Alexander Nohe <nohe@google.com>
2022-11-29 11:52:44 -0800

    Add in http request for logging

--- performance/lib/main.dart
+++ performance/lib/main.dart
@@ -12,12 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_performance/firebase_performance.dart';
 import 'package:flutter/material.dart';
+import 'package:performance_quickstart/firebase_status.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 import './firebase_options.dart';
 
@@ -80,6 +85,15 @@ class MyHomePage extends StatelessWidget {
                 child: const Text('Get Random String'),
               ),
               Text(appState.randomString),
+              ElevatedButton(
+                onPressed: () => {appState.getFirebaseStatus()},
+                child: const Text('Get Firebase Status'),
+              ),
+              InkWell(
+                child: Text(appState.lastIncident),
+                onTap: () => appState
+                    ._launchInWebViewOrVC(Uri.parse(appState.lastIncidentUri)),
+              )
             ],
           ),
         ),
@@ -96,12 +110,23 @@ class ApplicationState extends ChangeNotifier {
   String _randomString = '';
   String get randomString => _randomString;
 
+  final _firebaseStatusURL =
+      'https://status.firebase.google.com/incidents.json';
+  final _firebaseStatusBaseUri = 'https://status.firebase.google.com';
+
+  String _lastIncident = '';
+  String get lastIncident => _lastIncident;
+
+  String _lastIncidentUri = '';
+  String get lastIncidentUri => _lastIncidentUri;
+
   Future<void> init() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
 
     await FirebasePerformance.instance.setPerformanceCollectionEnabled(true);
+    HttpClient.enableTimelineLogging = true;
   }
 
   Future<void> updateRandomString() async {
@@ -118,5 +143,39 @@ class ApplicationState extends ChangeNotifier {
     _randomString = buffer.toString();
     notifyListeners();
     await trace.stop();
+  }
+
+  Future<void> getFirebaseStatus() async {
+    final metric = FirebasePerformance.instance
+        .newHttpMetric(_firebaseStatusBaseUri, HttpMethod.Get);
+
+    await metric.start();
+    final response = await http.get(Uri.parse(_firebaseStatusURL));
+    await metric.stop();
+
+    var responseString = response.body;
+    var jsonResponseString = '{ "response": $responseString }';
+
+    var responses = ResponseJson.fromJson(jsonDecode(jsonResponseString));
+
+    // Responses come in sorted, so getting the first element in the list is
+    // sufficient
+    if (responses.response != null) {
+      var status =
+          '${responses.response![0].externalDesc} \nLast updated: ${responses.response![0].modified}';
+      _lastIncident = status;
+      _lastIncidentUri =
+          '$_firebaseStatusBaseUri/${responses.response![0].uri}';
+      notifyListeners();
+    }
+  }
+
+  Future<void> _launchInWebViewOrVC(Uri url) async {
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.inAppWebView,
+    )) {
+      throw 'Could not launch $url';
+    }
   }
 }
