11ddf8f11e4443d9fecc7f4fdb18402f6d45644c
Alexander Nohe <nohe@google.com>
2022-11-30 13:10:50 -0800

    Update labels

--- admob/lib/main.dart
+++ admob/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AdMob Quickstart',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'AdMob Quickstart'),
     );
   }
 }
