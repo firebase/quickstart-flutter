8b436cc12669e8c71eb548e6ef738973866c00d2
Alexander Nohe <nohe427@users.noreply.github.com>
2022-12-19 18:50:19 -0500

    Admob Quickstart (#18)
    * Admob init
    
    * Flutter mobile ads
    
    * Show some ads
    
    * Working sample ads
    
    * Update README and default values
    
    * Update labels
    
    * Updated based on feedback

---
+++ admob/README.md
@@ -0,0 +1,62 @@
+AdMob by Google Quickstart
+=======================
+
+The AdMob by Google Flutter quickstart demonstrates how to display an interstitial ad and
+a banner ad.  AdRequest and AdView are used to display a banner ad
+and InterstitialAd is used to display the interstitial ad.
+
+Introduction
+------------
+
+- [Read more about AdMob by Google](https://firebase.google.com/docs/admob/)
+
+Getting Started
+---------------
+
+## Android
+
+- [Add AdMob to your Flutter Project](https://developers.google.com/admob/flutter/quick-start#android).
+- Configure your AdMob app id:
+  - In `android/app/src/main/AndroidManifest.xml` change the `ca-app-pub-xxxxxxxxxxxxxxxx~yyyyyyyyyy` value to your AdMob app id.
+- Run the sample on your Android device or emulator.
+- The running sample displays a test banner ad and a test interstitial add.
+
+## iOS
+
+- [Add AdMob to your Flutter Project](https://developers.google.com/admob/flutter/quick-start#ios).
+- Configure your AdMob app id:
+  - In `ios/Runner/Info.plist` change the `ca-app-pub-xxxxxxxxxxxxxxxx~yyyyyyyyyy` value to your AdMob app id.
+- Run the sample on your iOS device or simulator.
+- The running sample displays a test banner ad by default.
+- Press the button in the center of the screen to display a test interstitial ad.
+
+Result
+-----------
+<img src="docs/result.png" height="534" width="300"/>
+
+Support
+-------
+
+- [Stack Overflow](https://stackoverflow.com/questions/tagged/admob)
+- [Developer Forum](https://groups.google.com/group/google-admob-ads-sdk)
+- [Firebase Support](https://firebase.google.com/support/)
+
+License
+-------
+
+Copyright 2022 Google, Inc.
+
+Licensed to the Apache Software Foundation (ASF) under one or more contributor
+license agreements.  See the NOTICE file distributed with this work for
+additional information regarding copyright ownership.  The ASF licenses this
+file to you under the Apache License, Version 2.0 (the "License"); you may not
+use this file except in compliance with the License.  You may obtain a copy of
+the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+License for the specific language governing permissions and limitations under
+the License.
---
+++ admob/lib/main.dart
@@ -0,0 +1,203 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(ChangeNotifierProvider(
+    create: (context) => ApplicationState(),
+    builder: (context, child) => const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AdMob Quickstart',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'AdMob Quickstart'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Consumer<ApplicationState>(
+        builder: (context, appState, _) => Column(
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.all(64.0),
+              child: Image(
+                image: AssetImage('assets/logo-standard.png'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                  'AdMob is Googles mobile advertising platform that you'
+                  ' can use to generate revenue from your app. Using AdMob with '
+                  'Firebase provides you with additional app usage data and '
+                  'analytics capabilities.'),
+            ),
+            ElevatedButton(
+              onPressed: appState.interstitialAd != null
+                  ? () {
+                      appState.showInterstitial();
+                    }
+                  : null,
+              child: const Text('Load Interstitial'),
+            ),
+            const Expanded(child: SizedBox.shrink()),
+            appState.bannerAd != null
+                ? Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      width: appState.bannerAd!.size.width.toDouble(),
+                      height: appState.bannerAd!.size.height.toDouble(),
+                      child: AdWidget(ad: appState.bannerAd!),
+                    ),
+                  )
+                : const SizedBox.shrink(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ApplicationState extends ChangeNotifier {
+  ApplicationState() {
+    init();
+  }
+
+  BannerAd? _bannerAd;
+  BannerAd? get bannerAd => _bannerAd;
+
+  InterstitialAd? _interstitialAd;
+  InterstitialAd? get interstitialAd => _interstitialAd;
+
+  Future<void> init() async {
+    MobileAds.instance.initialize();
+
+    // // Examples of all the listeners that can be called and acted upon.
+    final BannerAdListener listener = BannerAdListener(
+      // Called when an ad is successfully received.
+      onAdLoaded: (Ad ad) {
+        print('Ad loaded.');
+        _bannerAd = ad as BannerAd?;
+        notifyListeners();
+      },
+      // Called when an ad request failed.
+      onAdFailedToLoad: (Ad ad, LoadAdError error) {
+        // Dispose the ad here to free resources.
+        ad.dispose();
+        print('Ad failed to load: $error');
+      },
+      // Called when an ad opens an overlay that covers the screen.
+      onAdOpened: (Ad ad) => print('Ad opened.'),
+      // Called when an ad removes an overlay that covers the screen.
+      onAdClosed: (Ad ad) => print('Ad closed.'),
+      // Called when an impression occurs on the ad.
+      onAdImpression: (Ad ad) => print('Ad impression.'),
+    );
+
+    loadBannerAd(listener);
+    loadInterstitialAd();
+  }
+
+  String getBannerAdUnitId() {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544/6300978111';
+    }
+    // return an iOS ad unit
+    return 'ca-app-pub-3940256099942544/2934735716';
+  }
+
+  String getInterstitialAdUnit() {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544/1033173712';
+    }
+    // return an iOS ad unit
+    return 'ca-app-pub-3940256099942544/4411468910';
+  }
+
+  void loadBannerAd(BannerAdListener listener) {
+    print('loading banner ad');
+    final BannerAd myBanner = BannerAd(
+      adUnitId: getBannerAdUnitId(),
+      size: AdSize.banner,
+      request: const AdRequest(),
+      listener: listener,
+    );
+    myBanner.load();
+  }
+
+  void loadInterstitialAd() {
+    InterstitialAd.load(
+      adUnitId: getInterstitialAdUnit(),
+      request: const AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (InterstitialAd ad) {
+          // Keep a reference to the ad so you can show it later.
+          fullscreenContentCallback(ad);
+          _interstitialAd = ad;
+          notifyListeners();
+        },
+        onAdFailedToLoad: (LoadAdError error) {
+          print('InterstitialAd failed to load: $error');
+        },
+      ),
+    );
+  }
+
+  void fullscreenContentCallback(InterstitialAd ad) {
+    ad.fullScreenContentCallback = FullScreenContentCallback(
+      onAdShowedFullScreenContent: (InterstitialAd ad) =>
+          print('%ad onAdShowedFullScreenContent.'),
+      onAdDismissedFullScreenContent: (InterstitialAd ad) {
+        print('$ad onAdDismissedFullScreenContent.');
+        ad.dispose();
+        loadInterstitialAd();
+      },
+      onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
+        print('$ad onAdFailedToShowFullScreenContent: $error');
+        ad.dispose();
+        loadInterstitialAd();
+      },
+      onAdImpression: (InterstitialAd ad) => print('$ad impression occurred.'),
+    );
+  }
+
+  void showInterstitial() {
+    if (interstitialAd != null) {
+      interstitialAd!.show();
+    }
+  }
+}
