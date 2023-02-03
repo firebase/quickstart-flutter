fe793d1ebe67d24dbab54b329b491617b70b424a
Alexander Nohe <nohe@google.com>
2023-02-03 11:16:06 -0800

    Add in Firebase support

--- analytics/lib/main.dart
+++ analytics/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized;
+
   runApp(ChangeNotifierProvider(
     create: (context) => ApplicationState(),
     builder: (context, child) => const MyApp(),
--- analytics/macos/Flutter/GeneratedPluginRegistrant.swift
+++ analytics/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
