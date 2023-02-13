099df2fc8755ff1e8a72387e046441a79cebebf6
Alexander Nohe <nohe@google.com>
2023-02-13 12:05:11 -0800

    Default Project Setup.

--- cloud_functions/macos/Flutter/GeneratedPluginRegistrant.swift
+++ cloud_functions/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_functions
+import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
