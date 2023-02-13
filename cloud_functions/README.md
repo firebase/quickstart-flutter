d2ae033d012c1d8237ac049f75027f1eccc2e2da
Alexander Nohe <nohe@google.com>
2023-02-13 15:56:34 -0800

    Cloud functions update

--- cloud_functions/README.md
+++ cloud_functions/README.md
@@ -8,11 +8,9 @@ You will want to call `flutterfire configure` before running this code so your p
 
 ## firebase_functions
 
-In testing firebase_functions, we encourage you to try using the [emulators](https://firebase.google.com/docs/emulator-suite) before deploying to production. We have written a V2 function and a V1 function for comparison. If you are interested in handling multiple requests concurrently and the power of [eventarc](https://cloud.google.com/eventarc/docs) + [cloudrun](https://cloud.google.com/run) function enviornments, we encourage you to look into [V2 functions](https://firebase.google.com/docs/functions/beta).
+In testing firebase_functions, we encourage you to try using the [emulators](https://firebase.google.com/docs/emulator-suite) before deploying to production. We have written a V1 function for your use. If you are interested in handling multiple requests concurrently and the power of [eventarc](https://cloud.google.com/eventarc/docs) + [cloudrun](https://cloud.google.com/run) function enviornments, we encourage you to look into [V2 functions](https://firebase.google.com/docs/functions/beta).
 
-To start running the functions locally in this folder, change into the `firebase_functions/functions` folder and run `npm run serve`. This will automatically start the emulator suite to test the functions locally.
-
-We have optionally added the code to manage auth state and appcheck state in the functions but have left those commented out. AppCheck will allow you to verify that function requests are coming from authorized applications while auth ensures that users are authorized users of your application.
+To start running the functions locally in this folder, run `firebase emulators:start`. This will automatically start the emulator suite to test the functions locally.
 
 ## Dart code
 
@@ -21,7 +19,7 @@ The dart code invokes the function via:
 ```dart
 FirebaseFunctions.instance
     .httpsCallable(
-        'callablevtwo',
+        'addMessage',
     )
     .call();
 ```
--- cloud_functions/lib/main.dart
+++ cloud_functions/lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
-Future<void> main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
-  // Comment out the following to run against production
-  FirebaseFunctions.instance.useFunctionsEmulator("localhost", 5001);
-
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ApplicationState(),
+    builder: (context, child) => const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -25,80 +25,230 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Firebase Functions Quickstart'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final _controller = TextEditingController();
-  String _response = "";
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: TextFormField(
-                controller: _controller,
-                decoration: const InputDecoration(
-                  hintText: 'Enter a message',
+      body: Column(
+        children: [
+          Card(
+            elevation: 4.0,
+            margin: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const Text('Add Two Numbers'),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: 64.0,
+                        child: Consumer<ApplicationState>(
+                          builder: (context, appState, child) => TextField(
+                            onChanged: (value) => appState.mathInputOne = value,
+                            textAlign: TextAlign.center,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              border: UnderlineInputBorder(),
+                            ),
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Text('+'),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: 64.0,
+                        child: Consumer<ApplicationState>(
+                          builder: (context, appState, child) => TextField(
+                            onChanged: (value) => appState.mathInputTwo = value,
+                            textAlign: TextAlign.center,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              border: UnderlineInputBorder(),
+                            ),
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly,
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Text('='),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: 64.0,
+                        child: Consumer<ApplicationState>(
+                          builder: (context, appState, child) =>
+                              Text(appState.mathOutput),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
+                Consumer<ApplicationState>(
+                  builder: (context, appState, child) {
+                    return ElevatedButton(
+                      onPressed: () => {appState.callAddNumbers()},
+                      child: const Text('Calculate'),
+                    );
+                  },
+                ),
+              ],
             ),
-            OutlinedButton(
-              onPressed: () {
-                FirebaseFunctions.instance
-                    .httpsCallable(
-                  'callablevtwo',
-                  options: HttpsCallableOptions(
-                    timeout: const Duration(
-                      seconds: 60,
+          ),
+          Card(
+            elevation: 4.0,
+            margin: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const Text('Sanitize a Message'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    child: Consumer<ApplicationState>(
+                      builder: (context, appState, child) => TextField(
+                        onChanged: (value) => appState.sanatizeInput = value,
+                        decoration: const InputDecoration(
+                          labelText: 'Input',
+                          border: UnderlineInputBorder(),
+                        ),
+                      ),
                     ),
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    child: Consumer<ApplicationState>(
+                      builder: (context, appState, child) => Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Output: ${appState.sanatizeOutput}',
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Consumer<ApplicationState>(
+                          builder: (context, appState, child) => Row(
+                                children: [
+                                  ElevatedButton(
+                                    onPressed: appState.signedIn
+                                        ? null
+                                        : () => {appState.signIn()},
+                                    child: const Text('Sign In'),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () =>
+                                        {appState.callAddMessage()},
+                                    child: const Text('Sanitize'),
+                                  ),
+                                ],
+                              )),
+                    ),
+                  ],
                 )
-                    .call(
-                  {
-                    "word": _controller.value.text,
-                  },
-                ).then((result) {
-                  setState(() {
-                    _response = result.data as String;
-                  });
-                }).catchError((err) {
-                  if (err is FirebaseFunctionsException) {
-                    debugPrint(err.code);
-                    debugPrint(err.details);
-                    debugPrint(err.message);
-                  } else {
-                    debugPrint(err.toString());
-                  }
-                });
-              },
-              child: const Text("Callable V2"),
+              ],
             ),
-            Text(
-              _response,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class ApplicationState extends ChangeNotifier {
+  ApplicationState() {
+    init();
+  }
+
+  String mathInputOne = '';
+  String mathInputTwo = '';
+  String mathOutput = '';
+
+  String sanatizeInput = '';
+  String sanatizeOutput = '';
+
+  bool _signedIn = false;
+  bool get signedIn => _signedIn;
+
+  late FirebaseFunctions functions;
+  late HttpsCallable addNumbers;
+  late HttpsCallable addMessage;
+  late FirebaseAuth auth;
+
+  Future<void> init() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    // Uncomment out the following to run against an emulator
+    FirebaseFunctions.instance.useFunctionsEmulator("localhost", 5001);
+
+    functions = FirebaseFunctions.instance;
+    auth = FirebaseAuth.instance;
+    addNumbers = functions.httpsCallable('addNumbers');
+    addMessage = functions.httpsCallable('addMessage');
+
+    auth.authStateChanges().listen((event) {
+      if (auth.currentUser != null) {
+        _signedIn = true;
+      } else {
+        _signedIn = false;
+      }
+      notifyListeners();
+    });
+  }
+
+  void callAddNumbers() {
+    addNumbers.call(
+      {
+        'firstNumber': double.parse(mathInputOne),
+        'secondNumber': double.parse(mathInputTwo),
+      },
+    ).then((value) => {
+          mathOutput = value.data['operationResult'].toString(),
+          notifyListeners(),
+        });
+  }
+
+  void callAddMessage() {
+    addMessage.call(
+      {
+        'text': sanatizeInput,
+      },
+    ).then((value) => {
+          debugPrint(value.data.toString()),
+          sanatizeOutput = value.data.toString(),
+          notifyListeners(),
+        });
+  }
+
+  void signIn() {
+    auth.signInAnonymously();
   }
 }
