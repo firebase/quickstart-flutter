4bdd4b55b0d8a0975c8bc1475baff39c5cae926f
Maneesh Tewani <mtewani@google.com>
2024-12-12 07:39:32 -0800

    Fixed warnings

--- data_connect/lib/sign_up.dart
+++ data_connect/lib/sign_up.dart
@@ -1,7 +1,6 @@
 import 'package:dataconnect/movies_connector/movies.dart';
 import 'package:dataconnect/widgets/auth_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
