31c0b742a9a6fb153db85badf6fcd568f409e135
Maneesh Tewani <mtewani@google.com>
2025-02-07 13:40:03 -0800

    Brought back windows and ios

--- data_connect/run-idx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for file in `find . -name "*.idx.*" -type f`
-do
-    [ -e "$file" ] || continue
-    echo $file
-    filename=${file%.idx*}
-    extension="${file#*.idx}"
-    mv "$file" "${filename}${extension}"
-done
