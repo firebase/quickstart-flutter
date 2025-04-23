3a125d1e1e5a6d0aae74f2f6bd26b9b60aba172c
Cynthia J <cynthiajoan@gmail.com>
2025-04-22 20:56:53 -0700

    Update vertexai quick starter

--- vertexai/macos/Flutter/GeneratedPluginRegistrant.swift
+++ vertexai/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,22 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import firebase_app_check
+import firebase_auth
 import firebase_core
+import firebase_storage
+import just_audio
+import path_provider_foundation
+import record_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
 }
